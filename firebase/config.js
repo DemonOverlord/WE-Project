b59f44469334d546796e01b7f738722ea473bc65
DemonOverlord <mpire745@hotmail.com>
2024-04-08 23:03:39 +0530

    fixed config

--- firebase/config.js
+++ firebase/config.js
@@ -1,30 +1,30 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
-import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBCj0TGUB9FjtNUOqkreTNjPC5l28bUCMk",
-  authDomain: "weproject-c52d8.firebaseapp.com",
-  projectId: "weproject-c52d8",
-  storageBucket: "weproject-c52d8.appspot.com",
-  messagingSenderId: "78456879287",
-  appId: "1:78456879287:web:9c513be62d9f1efd4cf532"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig)
+initializeApp(firebaseConfig);
 
 // Initialize Firestore
-const db = getFirestore()
+const db = getFirestore();
 
 // Intialize Auth
-const auth = getAuth()
+const auth = getAuth();
 
 // Initialize Google Auth Provider
-const provider = new GoogleAuthProvider()
+const provider = new GoogleAuthProvider();
 
 // Timestamp
 // const timestamp = Timestamp
 
 //  timestamp.now()
-export { db, auth, provider }
+export { db, auth, provider };
