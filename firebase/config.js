fdd2ae1a239f751c0a01d20e1ffce97a074394a4
DemonOverlord <mpire745@hotmail.com>
2024-04-24 23:30:51 +0530

    FIxed Config

--- firebase/config.js
+++ firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBCj0TGUB9FjtNUOqkreTNjPC5l28bUCMk",
-  authDomain: "weproject-c52d8.firebaseapp.com",
-  projectId: "weproject-c52d8",
-  storageBucket: "weproject-c52d8.appspot.com",
-  messagingSenderId: "78456879287",
-  appId: "1:78456879287:web:9c513be62d9f1efd4cf532",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
